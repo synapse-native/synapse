cf68b2fc5ade82eba0c0b8031fd7075f12037be7
gedeon1972-svg <gedeon1972-svg@users.noreply.github.com>
2026-07-13 13:12:34 +0000

    deploy: 4a3ef836f3381cc49db71a5d2c370ea174a0f5b5

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-edc91bf8.js',
+                path_to_root + 'searchindex-e5460344.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
